31f27fd359254691d4f9c1389bc6955d0cf60e45
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-03-02 14:13:41 -0300

    feat: create Transaction entity

---
+++ PicpayAPI/src/main/java/io/github/mateusbosquetti/picpayapi/model/entity/Transaction.java
@@ -0,0 +1,39 @@
+package io.github.mateusbosquetti.picpayapi.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Builder
+@Entity
+@Table(name = "transactions")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private User receiver;
+
+    @Column(nullable = false)
+    private LocalDateTime timeStamp;
+
+}
