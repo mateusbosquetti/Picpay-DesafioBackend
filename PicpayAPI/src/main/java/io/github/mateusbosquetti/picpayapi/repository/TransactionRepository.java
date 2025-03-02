326908c932a3f598d683b82fcb687b3329d84e48
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-03-02 14:14:42 -0300

    feat: create TransactionRepository

---
+++ PicpayAPI/src/main/java/io/github/mateusbosquetti/picpayapi/repository/TransactionRepository.java
@@ -0,0 +1,8 @@
+package io.github.mateusbosquetti.picpayapi.repository;
+
+import io.github.mateusbosquetti.picpayapi.model.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    
+}
