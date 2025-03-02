322eae80ee342eda3bbe5af9dc883e9d7861f57c
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-03-02 14:13:15 -0300

    feat:  initial config

---
+++ PicpayAPI/src/main/java/io/github/mateusbosquetti/picpayapi/PicpayApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.mateusbosquetti.picpayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicpayApiApplication.class, args);
+    }
+
+}
---
+++ PicpayAPI/src/test/java/io/github/mateusbosquetti/picpayapi/PicpayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mateusbosquetti.picpayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
